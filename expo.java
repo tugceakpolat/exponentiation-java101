0c3b49cda265130c162d494f5f8433102459cc27
Tuğçe <tugce.okcu@hotmail.com>
2023-01-10 15:03:44 +0300

    error

--- expo.java
+++ expo.java
@@ -5,6 +5,7 @@ public class expo {
         int take;
         int be;
         int total=1;
+        boolean isError = true;
 
         Scanner input = new Scanner(System.in);
 
@@ -14,12 +15,18 @@ public class expo {
         System.out.print("Enter the number to be the exponent : ");
         be = input.nextInt();
 
-        for (int start =1; start <= be; start++){
-            total *= take;
+        if(take < 0 ){
+            System.out.println("Please enter a pozitive number.");
+            isError = false;
+        }else {
+
+            for (int start = 1; start <= be; start++) {
+                total *= take;
+
+            }
+            System.out.println("Answer : " + total);
+
         }
-        System.out.println("Answer : " + total);
-
-
 
     }
 }
